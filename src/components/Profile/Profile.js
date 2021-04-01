84f46c0e5589f22eafeeb1a451671ef8b9f081b8
sebastianlares92 <64242425+sebastianlares92@users.noreply.github.com>
2021-04-01 13:56:43 -0300

    componente profile editado

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,18 +5,13 @@ function Profile() {
   return (
     <section className="profile-container">
       <div className="profile">
-        <div className="profile-picture">
-          <div className="picture" />
-        </div>
-        <div className="profile-description">
-          <h3>Hi, I'm Sebastian Lares</h3>
-          <p>
-            Front End Developer from Buenos Aires, Argentina, and making new
-            projects and websites is my passion. I specialize in JavaScript and
-            React.js, but I'm always learning new technologies and ways to make
-            simpler and cleaner applications.
-          </p>
-        </div>
+        <h3>Hi, my name is Sebastian Lares</h3>
+        <p>
+          I'm a Front End Developer from Buenos Aires, Argentina, and making new
+          projects and websites is my passion. I specialize in JavaScript and
+          React.js, but I'm always learning new technologies and ways to make
+          simpler and cleaner applications.
+        </p>
       </div>
     </section>
   );
